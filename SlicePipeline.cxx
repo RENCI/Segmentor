88b14a8b0e6d67d03acef21f1006987e6321cfcc
David Borland <borland@renci.org>
2018-04-17 16:45:22 -0400

    Add mouse click callbacks to slice view

--- SlicePipeline.cxx
+++ SlicePipeline.cxx
@@ -1,16 +1,42 @@
 #include "SlicePipeline.h"
 
 #include <vtkActor.h>
+#include <vtkCallbackCommand.h>
 #include <vtkImageData.h>
 #include <vtkImageProperty.h>
 #include <vtkImageResliceMapper.h>
 #include <vtkImageSlice.h>
 #include <vtkInteractorStyleImage.h>
+#include <vtkObject.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkImageProperty.h>
 #include <vtkImageSlice.h>
+
+// XXX: Probably want to create a new interactor style derived from vtkInteractorStyleImage
+int oldX;
+int oldY;
+
+void onLeftButtonPress(vtkObject* caller, unsigned long eventId, void* clientData, void* callData) {
+	vtkInteractorStyle* style = vtkInteractorStyle::SafeDownCast(caller);
+	style->GetInteractor()->GetEventPosition(oldX, oldY);
+}
+
+void onLeftButtonRelease(vtkObject* caller, unsigned long eventId, void* clientData, void* callData) {
+	vtkInteractorStyle* style = vtkInteractorStyle::SafeDownCast(caller);
+
+	int x;
+	int y;
+
+	style->GetInteractor()->GetEventPosition(x, y);
+
+	cout << oldX << " " << x << " " << oldY << " " << y << endl;
+
+	if (x == oldX && y == oldY) {
+		cout << "CLICK" << endl;
+	}
+}
 
 SlicePipeline::SlicePipeline(vtkRenderWindowInteractor* rwi) {
 	this->renderer = vtkSmartPointer<vtkRenderer>::New();
@@ -43,10 +69,21 @@ void SlicePipeline::SetInput(vtkImageData* input) {
 	slice->SetMapper(mapper);
 	slice->SetProperty(property);
 
+	// Callbacks
+	vtkSmartPointer<vtkCallbackCommand> leftButtonPressCallback = vtkSmartPointer<vtkCallbackCommand>::New();
+	leftButtonPressCallback->SetCallback(onLeftButtonPress);
+
+	vtkSmartPointer<vtkCallbackCommand> leftButtonReleaseCallback = vtkSmartPointer<vtkCallbackCommand>::New();
+	leftButtonReleaseCallback->SetCallback(onLeftButtonRelease);
+
 	// Interaction
 	vtkSmartPointer<vtkInteractorStyleImage> style = vtkSmartPointer<vtkInteractorStyleImage>::New();
 	style->SetInteractionModeToImage3D();
-	renderer->GetRenderWindow()->GetInteractor()->SetInteractorStyle(style);
+	style->AddObserver(vtkCommand::LeftButtonPressEvent, leftButtonPressCallback, 1.0);
+	style->AddObserver(vtkCommand::LeftButtonReleaseEvent, leftButtonReleaseCallback, 1.0);
+
+	vtkRenderWindowInteractor* interactor = renderer->GetRenderWindow()->GetInteractor();
+	interactor->SetInteractorStyle(style);
 
 	// Render
 	renderer->AddActor(slice);
